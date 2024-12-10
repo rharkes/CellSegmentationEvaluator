f44488a9afeb5da91beac39417d4f8683ee53eb4
r.harkes <r.harkes@nki.nl>
2024-12-10 13:29:17 +0100

    change url

--- pip_package/setup.py
+++ pip_package/setup.py
@@ -10,7 +10,7 @@ setup(
     description='Functions for reference-free evaluation of the quality of cell segmentations',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/murphylab/CellSegmentationEvaluator/',
+    url='https://github.com/rharkes/CellSegmentationEvaluator',
     author='Haoran Chen and Ce Zhang and Robert F. Murphy',
     author_email='murphy@cmu.edu',
     license='MIT',
